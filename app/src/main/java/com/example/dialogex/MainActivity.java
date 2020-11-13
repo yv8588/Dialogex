174d38cc1a76ad0148a05161879931eb60e459bc
Yoad <yv8588@bs.k12.amalnet.il>
2020-11-13 09:40:45 +0200

    done all buttons missing credits activity. icon still does not work.

--- app/src/main/java/com/example/dialogex/MainActivity.java
+++ app/src/main/java/com/example/dialogex/MainActivity.java
@@ -8,11 +8,18 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+
+import java.util.Random;
+
+import static android.graphics.Color.WHITE;
 
 public class MainActivity extends AppCompatActivity {
 AlertDialog.Builder ad;
+LinearLayout s;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +27,8 @@ AlertDialog.Builder ad;
     }
     /**
      * when the button got clicked shows an alert dialog with  text
+     * <p>
+     *
      * @param view the button that got clicked
      */
     public void textad(View view) {
@@ -31,26 +40,28 @@ AlertDialog.Builder ad;
     }
     /**
      * when the button got clicked shows an alert dialog with image
+     * <p>
+     *
      * @param view the button that got clicked
      */
     public void photo(View view) {
         ad=new AlertDialog.Builder(this);
-        ad.setTitle("hello");
         ad.setMessage("hi");
         ad.setIcon(R.drawable.exercies);
         AlertDialog adb=ad.create();
         adb.show();
-
     }
 
     /**
      * when the button got clicked shows an alert dialog with image text and button
+     * <p>
+     *
      * @param view the button that got clicked
      */
     public void button(View view) {
         ad=new AlertDialog.Builder(this);
-        ad.setMessage("hi");
         ad.setIcon(R.drawable.exercies);
+        ad.setMessage("hi");
         ad.setPositiveButton("ok", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
@@ -61,4 +72,87 @@ AlertDialog.Builder ad;
         adb.show();
     }
 
+    /**
+     * when the button got clicked sets an alert dialog that can either generate random screen color or cancel
+     * <p>
+     *
+     * @param view the button that got clicked
+     */
+    public void randomcolor(View view) {
+        ad.setMessage("random color");
+        ad.setPositiveButton("random", new DialogInterface.OnClickListener() {
+            @Override
+        /**
+          * when the ok got clicked on the dialog sets the screen at random color
+         * <p>
+         *
+         * @param view the button that got clicked  on the dialog alert
+         */
+            public void onClick(DialogInterface dialog, int which) {
+                Random rnd = new Random();
+                int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+                s.setBackgroundColor(color);
+            }
+        });
+        ad.setNegativeButton("out", new DialogInterface.OnClickListener() {
+            @Override
+            /**
+             * when the out got clicked on the dialog get out
+             * <p>
+             *
+             * @param view the button that got clicked  on the dialog alert
+             */
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+    }
+
+    /**
+     * when the button got clicked shows alert dialog with random option rest background option an out
+     * <p>
+     * @param view the button that got clicked
+     */
+    public void whiteop(View view) {
+        ad.setMessage("set color");
+        ad.setPositiveButton("random", new DialogInterface.OnClickListener() {
+            @Override
+            /**
+             * when the ok got clicked on the dialog sets the screen at random color
+             * <p>
+             *
+             * @param view the button that got clicked  on the dialog alert
+             */
+            public void onClick(DialogInterface dialog, int which) {
+                Random rnd = new Random();
+                int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+                s.setBackgroundColor(color);
+            }
+        });
+        ad.setNeutralButton("restart", new DialogInterface.OnClickListener() {
+            @Override
+            /**
+             * when the reset  got clicked on the dialog sets background back to white
+             * <p>
+             *
+             * @param view the button that got clicked  on the dialog alert
+             */
+            public void onClick(DialogInterface dialog, int which) {
+                s.setBackgroundColor(WHITE);
+            }
+        });
+        ad.setNegativeButton("out", new DialogInterface.OnClickListener() {
+            @Override
+            /**
+             * when the out got clicked on the dialog get out
+             * <p>
+             *
+             * @param view the button that got clicked  on the dialog alert
+             */
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+    }
 }
